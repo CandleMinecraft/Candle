ac1e336e2748a2904814ef2774a936487ff88cf8
Nelmin <nelmindev@proton.me>
2025-03-27 01:39:01 +0100

    Added Copyright to Logger

--- modules/base/logger/src/main/kotlin/net/candlemc/logger/Logger.kt
+++ modules/base/logger/src/main/kotlin/net/candlemc/logger/Logger.kt
@@ -5,20 +5,45 @@ import java.nio.file.Path
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 
-class Logger @JvmOverloads constructor(// Is the Logger in Debug Mode.
-    private val debug: Boolean = false, // Name of the Module, Plugin and so on.
-    private val name: String = "CandleMC"
-) {
+/**
+ * Copyright 2025 © Nelmin
+ * I hereby permit anyone to use this Logger when and where they want to, including myself.
+ */
+object Logger {
     init {
         Files.createDirectories(Path.of(System.getProperty("user.dir") + "/logs"))
     }
 
+    private var debug: Boolean = System.getProperty("debugMode")?.toBoolean() ?: false // Is the Logger in Debug Mode.
+    private var name: String = "CandleMC" // Name of the Module, Plugin and so on.
+
+    // Single Thread Executor for all logging operations
+    private val executor: ExecutorService = Executors.newSingleThreadExecutor()
+    private var debugStrategy: DebugLoggingStrategy = DebugLoggingStrategy(executor, debug)
+    private val errorStrategy: LoggingStrategy = CandleLoggingStrategy(
+        "error", "ERROR", executor, ANSIColors.RED
+    )
+    private val fatalStrategy: LoggingStrategy = CandleLoggingStrategy(
+        "error", "FATAL", executor, ANSIColors.BOLD_RED
+    )
+    private val infoStrategy: LoggingStrategy = CandleLoggingStrategy(
+        "info", "INFO", executor, ANSIColors.CYAN
+    )
+    private val stackTraceStrategy: LoggingStrategy = StackTraceLoggingStrategy(executor)
+    private val warnStrategy: LoggingStrategy = CandleLoggingStrategy(
+        "info", "WARN", executor, ANSIColors.YELLOW
+    )
+
+    fun setName(name: String) {
+        this.name = name
+    }
+
     fun log(strategy: LoggingStrategy, vararg content: Any) {
-        val FORMAT_NORMAL = "[%timestamp] - %log_level - %name - %content"
-        val FORMAT_DEBUG = "[%timestamp] - %log_level - %name - %package:%line - %content"
+        val format_normal = "[%timestamp] - %log_level - %name - %content"
+        val format_debug = "[%timestamp] - %log_level - %name - %package:%line - %content"
         strategy.log(
             this.name,
-            if (debug) FORMAT_DEBUG else FORMAT_NORMAL,
+            if (debug) format_debug else format_normal,
             System.currentTimeMillis(),
             *content
         )
@@ -46,26 +71,5 @@ class Logger @JvmOverloads constructor(// Is the Logger in Debug Mode.
 
     fun warn(vararg content: Any) {
         log(warnStrategy, *content)
-    }
-
-    companion object {
-        // Single Thread Executor for all logging operations
-        private val executor: ExecutorService = Executors.newSingleThreadExecutor()
-        private val debugStrategy: LoggingStrategy = CandleLoggingStrategy(
-            "debug", "DEBUG", executor, ANSIColors.GREEN
-        )
-        private val errorStrategy: LoggingStrategy = CandleLoggingStrategy(
-            "error", "ERROR", executor, ANSIColors.RED
-        )
-        private val fatalStrategy: LoggingStrategy = CandleLoggingStrategy(
-            "error", "FATAL", executor, ANSIColors.BOLD_RED
-        )
-        private val infoStrategy: LoggingStrategy = CandleLoggingStrategy(
-            "info", "INFO", executor, ANSIColors.CYAN
-        )
-        private val stackTraceStrategy: LoggingStrategy = StackTraceLoggingStrategy(executor)
-        private val warnStrategy: LoggingStrategy = CandleLoggingStrategy(
-            "info", "WARN", executor, ANSIColors.YELLOW
-        )
     }
 }
